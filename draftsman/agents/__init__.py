41f019a8530657e0616d421351f93c9f664786ba
skrider <skrider@berkeley.edu>
2023-12-08 08:23:06 +0000

    add spec config

--- draftsman/agents/__init__.py
+++ draftsman/agents/__init__.py
@@ -1,15 +1,9 @@
-from .awac_agent import AWACAgent
-from .cql_agent import CQLAgent
 from .dqn_agent import DQNAgent
 from .random_agent import RandomAgent
-from .iql_agent import IQLAgent
 from .rnd_agent import RNDAgent
 
 agents = {
     "random": RandomAgent,
     "dqn": DQNAgent,
-    "cql": CQLAgent,
-    "awac": AWACAgent,
     "rnd": RNDAgent,
-    "iql": IQLAgent,
 }
