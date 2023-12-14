7d13b335031a295e03cba293c03ab0faff9e5f7a
skrider <skrider@berkeley.edu>
2023-12-14 04:36:25 +0000

    add config

--- draftsman/envs/__init__.py
+++ draftsman/envs/__init__.py
@@ -147,3 +147,40 @@ gym.register(
         "one_hot_encode_prev": False
     },
 )
+
+gym.register(
+    id="SpeculativeDecoding-v1",
+    entry_point=SpeculativeDecoding,
+    kwargs={
+        "conversations_paths": ["datasets/out_0.parquet", "datasets/out_1.parquet"],
+        "n_conversations": 5000,
+        "conversation_offset": 0,
+        "max_tokens_guess": 16,
+        "max_tokens": 192,
+        "accepted_tokens_weight": 1.0,
+        "rejected_tokens_weight": 1.0,
+        "logarithmic": False,
+        "use_main_hidden_states": False,
+        "use_draft_hidden_states": True,
+        "one_hot_encode_prev": False
+    },
+)
+
+gym.register(
+    id="SpeculativeDecoding-v1",
+    entry_point=SpeculativeDecoding,
+    kwargs={
+        "conversations_paths": ["datasets/out_2.parquet", "datasets/out_3.parquet"],
+        "n_conversations": 5000,
+        "conversation_offset": 0,
+        "max_tokens_guess": 16,
+        "max_tokens": 192,
+        "accepted_tokens_weight": 1.0,
+        "rejected_tokens_weight": 1.0,
+        "logarithmic": False,
+        "use_main_hidden_states": False,
+        "use_draft_hidden_states": True,
+        "one_hot_encode_prev": False
+    },
+)
+
